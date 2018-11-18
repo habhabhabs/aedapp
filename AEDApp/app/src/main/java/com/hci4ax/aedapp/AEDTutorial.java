7922d8d92a82d9268666e32c5d232af3a4abef92
Alex <habsters@hotmail.com>
2018-11-18 14:51:05 +0800

    successful dragdrop of aed pads

--- AEDApp/app/src/main/java/com/hci4ax/aedapp/AEDTutorial.java
+++ AEDApp/app/src/main/java/com/hci4ax/aedapp/AEDTutorial.java
@@ -1,12 +1,21 @@
 package com.hci4ax.aedapp;
 
+import android.content.ClipData;
 import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AEDTutorial extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,53 @@ public class AEDTutorial extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         // add code below
+        final TextView instPrompter = (TextView) findViewById(R.id.prompter);
+        final ImageView lpadDraggable = (ImageView) findViewById(R.id.lpadDraggableImg);
+        final ImageView rpadDraggable = (ImageView) findViewById(R.id.rpadDraggableImg);
+        final LinearLayout lpadDrop = (LinearLayout) findViewById(R.id.lpadDrop);
+        final LinearLayout rpadDrop = (LinearLayout) findViewById(R.id.rpadDrop);
+
+        // define draggables
+        final Handler handler = new Handler();
+        MediaPlayer.create(AEDTutorial.this, R.raw.speech1_hiimannie).start();
+        instPrompter.setText("Hi, I'm Annie.");
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                MediaPlayer.create(AEDTutorial.this, R.raw.speech2_welcometothistutorial).start();
+                instPrompter.setText("Welcome to this tutorial.");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        MediaPlayer.create(AEDTutorial.this, R.raw.speech3_inthistutorial).start();
+                        instPrompter.setText("In this tutorial");
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                MediaPlayer.create(AEDTutorial.this, R.raw.speech4_iamgoingtoshowyouhowtousetheaed).start();
+                                instPrompter.setText("I am going to show you how to use the AED.");
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Animation animation = new AlphaAnimation((float) 0.5, 0); // Change alpha from fully visible to invisible
+                                        animation.setDuration(500); // duration - half a second
+                                        animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
+                                        animation.setRepeatCount(5);
+                                        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+                                        lpadDraggable.startAnimation(animation);
+                                        rpadDraggable.startAnimation(animation);
+
+                                        MediaPlayer.create(AEDTutorial.this, R.raw.speech5_holdanddragthetwoaedpads).start();
+                                        instPrompter.setText("Hold and drag the two AED pads, to the body part as shown below.");
+                                        lpadDrop.setOnDragListener(dragListener);
+                                        rpadDrop.setOnDragListener(dragListener);
+                                        lpadDraggable.setOnLongClickListener(longClickListener);
+                                        rpadDraggable.setOnLongClickListener(longClickListener);
+                                    }
+                                }, 4000); }
+                        }, 1500); }
+                }, 2000); }
+        }, 2000);
 
     }
 
@@ -36,4 +92,38 @@ public class AEDTutorial extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
+
+    // when user initiate long click, start drag (L/R AED pads)
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, myShadowBuilder,v,0);
+            return true;
+        }
+    };
+
+    // when drag initiated, do following:
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            final View view = (View) event.getLocalState();
+            switch(dragEvent) {
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    LinearLayout oldparent = (LinearLayout) view.getParent();
+                    oldparent.removeView(view);
+                    LinearLayout newParent = (LinearLayout) v;
+                    newParent.addView(view);
+                    MediaPlayer.create(AEDTutorial.this, R.raw.click_sound).start();
+                    break;
+            }
+            return true;
+        }
+    };
 }
